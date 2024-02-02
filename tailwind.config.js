fbf62365579e73d44dcef8810cc9db38fe9122a2
Tharindu D <tharidudarshana678@gmail.com>
2024-02-02 13:10:32 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "dark-gray": "hsl(0, 0%, 55%)",
+        "very-dark-gray": "hsl(0, 0%, 41%)",
+      },
+
+      backgroundImage: {
+        "mobile-hero": "url('/images/mobile/image-hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
